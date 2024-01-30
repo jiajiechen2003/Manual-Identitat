e45159fbf4b1168653da563e2e72d9bce1f7c310
jiajiechen2003 <jchen2223@politecnics.barcelona>
2024-01-30 20:22:35 +0100

    fwf

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 const containers = document.querySelectorAll('.container-fluid');
-
 window.addEventListener('scroll', function() {
-   const triggerBottom =  window.innerHeight / 5 * 4;
-
-   containers.forEach(container => {
-    const containerTop = container.getBoundingClientRect().top;
-
-    if (containerTop < triggerBottom) {
-        container.classList.add('show');
-    } else {
-        container.classList.remove('show');
-    }
+    containers.forEach(container => {
+        const containerTop = container.getBoundingClientRect().top;
+        const isInViewPort = containerTop < (window.innerHeight * 0.8) && (containerTop + container.offsetHeight * 0.8) > 0;
+    container.classList.toggle('show', isInViewPort)
    })
 })
