2e3ab99de613574b503a175a16e2129926316535
jiajiechen2003 <jchen2223@politecnics.barcelona>
2024-01-31 18:39:06 +0100

    efavs

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ window.addEventListener('scroll', function() {
     container.classList.toggle('show', isInViewPort)
    })
 })
+
+function copiarAlPortapapeles(texto) {
+    // Crea un elemento de texto temporal
+    var elementoTemporal = document.createElement('textarea');
+    elementoTemporal.value = texto;
+  
+    // Agrega el elemento temporal al DOM
+    document.body.appendChild(elementoTemporal);
+  
+    // Selecciona y copia el texto dentro del elemento temporal
+    elementoTemporal.select();
+    document.execCommand('copy');
+  
+    // Elimina el elemento temporal del DOM
+    document.body.removeChild(elementoTemporal);
+  
+    // Puedes mostrar una notificación u otra respuesta aquí
+    alert('Texto copiado al portapapeles: ' + texto);
+  }
+  
