4378c4010e05762dda876839564d55571b9f7560
jiajiechen2003 <jchen2223@politecnics.barcelona>
2024-02-08 20:23:54 +0100

    klk

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-const containers = document.querySelectorAll('.container-fluid');
-window.addEventListener('scroll', function() {
-    containers.forEach(container => {
-        const containerTop = container.getBoundingClientRect().top;
-        const isInViewPort = containerTop < (window.innerHeight * 0.8) && (containerTop + container.offsetHeight * 0.8) > 0;
-    container.classList.toggle('show', isInViewPort)
-   })
-})
+const containers = document.querySelectorAll(".container-fluid");
+window.addEventListener("scroll", function () {
+  containers.forEach((container) => {
+    const containerTop = container.getBoundingClientRect().top;
+    const isInViewPort =
+      containerTop < window.innerHeight * 0.8 &&
+      containerTop + container.offsetHeight * 0.8 > 0;
+    container.classList.toggle("show", isInViewPort);
+  });
+});
 
-function copiarAlPortapapeles(texto) {
-    // Crea un elemento de texto temporal
-    var elementoTemporal = document.createElement('textarea');
-    elementoTemporal.value = texto;
-  
-    // Agrega el elemento temporal al DOM
-    document.body.appendChild(elementoTemporal);
-  
-    // Selecciona y copia el texto dentro del elemento temporal
-    elementoTemporal.select();
-    document.execCommand('copy');
-  
-    // Elimina el elemento temporal del DOM
-    document.body.removeChild(elementoTemporal);
-  
-    // Puedes mostrar una notificación u otra respuesta aquí
-    alert('Texto copiado al portapapeles: ' + texto);
-  }
-  
+function copiarAlPortapapeles(text) {
+  let copiedText = document.createElement("textarea");
+  let textContainer = document.getElementById("textContainer");
+
+  copiedText.value = text;
+
+  document.body.appendChild(copiedText);
+
+  copiedText.select();
+  document.execCommand("copy");
+
+  document.body.removeChild(copiedText);
+  textContainer.style.display = "flex";
+
+  setTimeout(function() {
+      textContainer.style.display = "none";
+   }, 2000);
+}
